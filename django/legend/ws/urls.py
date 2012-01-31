77b1f5124ea0f4d1ace831ef3cdea78e7284c89e
Jeff <jostendorf@gmail.com>
2012-01-30 23:02:39 -0600

    Added a README so that github will stop complaining, then built the rough draft of the webservices stuff, need to test this on the vps sometime before moving on

---
+++ django/legend/ws/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from legend import ws
+
+urlpatters = patterns('ws.views',
+   (r'^$', 'list'),
+   (r'^entry/$', 'Entry'),
+   (r'^entry/(?P<id>\d*)/$', 'Entry),
+)
