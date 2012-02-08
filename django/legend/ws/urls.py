45f1381c7305ec370dbb6f6ff4f8527153cf29fe
Jeff <jostendorf@gmail.com>
2012-02-08 17:50:33 +0000

    Updated web service calls, these support the admin rewrite and additions for further additions in development

--- django/legend/ws/urls.py
+++ django/legend/ws/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls.defaults import *
 from legend import ws
 
 urlpatterns = patterns('ws.views',
+   (r'^verify/$', 'verify'),
+
    (r'^$', 'list'),
    (r'^entry/$', 'Entry'),
    (r'^entry/(?P<id>\d*)/$', 'Entry'),
    (r'^tags/$', 'Tag'),
+   (r'^tags/(?P<id>\d*)/$', 'Tag')
 )
