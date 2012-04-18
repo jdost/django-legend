f3277597adf2ea3433c16440029d5f1866d6deb0
Jeff <jostendorf@gmail.com>
2012-04-18 10:03:56 -0400

    Some small fixes from the server, added a title to the About page and modified the footer, added twitter link and commented out nginx, for now.

--- django/legend/utils/views.py
+++ django/legend/utils/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from legend.utils import *
 
 def about(request):
-   return response(request, ABOUT, {})
+   return response(request, ABOUT, { "title": ABOUT['title'] })
 
 def admin(request):
    return render_to_response("admin.html", { "page": DEFAULT})
